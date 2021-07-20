e4b19011e6cba8b38003878260be5e55d0bbc3f2
esauortiz <esauortiz97@gmail.com>
2021-07-20 16:14:25 +0200

    set anchor pose implemented

---
+++ scripts/dwm1001_BleApi.py
@@ -0,0 +1,179 @@
+#!/usr/bin python3.6
+
+""" For more info on the documentation go to https://www.decawave.com/sites/default/files/dwm1001-api-guide.pdf
+"""
+import asyncio
+from bitstring import Bits
+from textwrap import wrap
+from bleak import discover, BleakClient
+
+class DWM1001_BLE_API_COMMANDS:
+        # Network Node Characteristics
+        LABEL                   = '0x2A00' # RW
+        OPERATION_MODE          = '3f0afd88-7770-46b0-b5e7-9fc099598964' # RW
+        NETWORK_ID              = '80f9d8bc-3bff-45bb-a181-2d6a37991208' # RW
+        LOCATION_DATA_MODE      = 'a02b947e-df97-4516-996a-1882521e0ead' # RW
+        LOCATION_DATA           = '003bbdf2-c634-4b3d-ab56-7ec889b89a37' # RO
+        PROXY_POSITIONS         = 'f4a67d7d-379d-4183-9c03-4b6ea5103291' # RO
+        DEVICE_INFO             = '1e63b1eb-d4ed-444e-af54-c1e965192501' # RO
+        STATISTICS              = '0eb2bc59-baf1-4c1c-8535-8a0204c69de5' # 
+        FW_UPDATE_PUSH          = '5955aa10-e085-4030-8aa6-bdfac89ac32b' # WO
+        FW_UPDATE_POLL          = '9eed0e27-09c0-4d1c-bd92-7c441daba850' # RO
+        DISCONNECT              = 'ed83b848-da03-4a0a-a2dc-8b401080e473' # WO
+        # Anchor-specific Characteristics
+        OPERATION_MODE          = '3f0afd88-7770-46b0-b5e7-9fc099598964' # RW
+        DEVICE_INFO             = '1e63b1eb-d4ed-444e-af54-c1e965192501' # RO
+        PERSISTED_POSITION      = 'f0f26c9b-2c8c-49ac-ab60-fe03def1b40c' # WO
+        MAC_STATS               = '28d01d60-89de-4bfa-b6e9-651ba596232c' # RO
+        CLUSTER_INFO            = '17b1613e-98f2-4436-bcde-23af17a10c72' # RO
+        ANCHOR_LIST             = '5b10c428-af2f-486f-aee1-9dbd79b6bccb' # RO
+        # Tag-specific Characteristics
+        OPERATION_MODE          = '3f0afd88-7770-46b0-b5e7-9fc099598964' # RO
+        UPDATE_RATE             = '7bd47f30-5602-4389-b069-8305731308b6' # RO
+
+class BleConnectionHandler(object):
+        def __init__(self):
+                self.loop = asyncio.get_event_loop()
+
+        def getDevices(self):
+                async def asyncGetDevices():
+                        print('Searching BT devices ...')
+                        devices = await discover()
+                        return devices
+
+                devices = self.loop.run_until_complete(asyncGetDevices())
+                return devices
+
+        def readFromDevice(self, address, UUID):
+                async def asyncReadFromDevice(address, UUID):
+                        async with BleakClient(address) as client:
+                                data = await client.read_gatt_char(UUID)
+                        return data
+
+                data = self.loop.run_until_complete(asyncReadFromDevice(address, UUID))
+                return data
+
+        def writeToDevice(self, address, UUID, data):
+                async def asyncWriteToDevice(address, UUID, data):
+                        async with BleakClient(address) as client:
+                                await client.write_gatt_char(UUID, data)
+                self.loop.run_until_complete(asyncWriteToDevice(address, UUID, data))
+
+        def send(self, address, msg_object):
+                """ send message over BLE
+                Parameters
+                ----------
+                msg_object : BleMsg
+                address: string
+                        BLE address
+                Returns
+                -------
+                """
+                msg_object.encodeBle()
+                self.writeToDevice(address, msg_object.UUID, msg_object.data)
+
+class BleMsg(object):
+        def __init__(self, api_command):
+                self.UUID  = api_command
+                self.data = []
+
+        def encodeBle(self, data):
+                """ Encode BLE msg, bytes slots are encoded 
+                as little endian as BLE spec suggests.
+                Parameters
+                ----------
+                data : (N,) array
+                Returns
+                -------
+                encoded_msg : bytearray
+                """                
+                return
+
+        def codeLittleEndian(self, bits):
+                """ Encode/decode bits as little endian bytes
+                Parameters
+                ----------
+                bits : (N,) Bits in hexadecimal
+                Returns
+                -------
+                encoded_msg : (N/8) array of bytes
+                """
+                bits = str(bits)
+                # assert bits in hexadecimal
+                if bits[0:2] != '0x':
+                        raise ValueError
+                # slice msg in pieces of 1 byte
+                wrapped_bits = wrap(str(bits)[2:], 2)
+                wrapped_bits.reverse()
+                return wrapped_bits
+
+        def listToByteArray(self, list):
+                """ set
+                Parameters
+                ----------
+                bits : (2, N) list of bytes
+                Returns
+                -------
+                encoded_msg : bitearray
+                """
+                # flatten list
+                # list = [item for sublist in list for item in sublist]
+                return bytes.fromhex(''.join(list))
+
+class PersistedPositionMsg(BleMsg):
+        def __init__(self, data):
+                """
+                Parameters
+                ----------
+                data : (3,) array
+                        x, y, z as signed int
+                """
+                BleMsg.__init__(self, DWM1001_BLE_API_COMMANDS.PERSISTED_POSITION)
+                self.data = data
+
+        def encodeBle(self):
+                """ Encode BLE msg, bytes slots are encoded 
+                as little endian as BLE spec suggests.
+                Parameters
+                ----------
+                Returns
+                -------
+                """
+                result = []
+                for coord in self.data:
+                        # 4 byte coord in mm
+                        coord = self.codeLittleEndian(Bits(int = int(float(coord) * 1000), length = 32))
+                        for element in coord:
+                                result.append(element)
+                # quality factor (1 byte, value 1 - 100 DEC)
+                result.append('64')
+                self.data = self.listToByteArray(result)
+
+class OperationModeMsg(BleMsg):
+        def __init__(self, data = None):
+                """
+                Parameters
+                ----------
+                data : dictionary
+                """
+                BleMsg.__init__(self, DWM1001_BLE_API_COMMANDS.OPERATION_MODE)
+                self.data = data
+
+        def encodeBle(self):
+                """ Encode BLE msg, bytes slots are encoded 
+                as little endian as BLE spec suggests.
+                Parameters
+                ----------
+                Returns
+                -------
+                """
+                # read 'data' dictionary to build msg
+                # 2 bytes non little endian see API documentation
+
+        def decodeBle(self):
+                """ TODO decode msg
+                Parameters
+                ----------
+                Returns
+                -------
+                """
--- scripts/dwm_cfg.py
+++ scripts/dwm_cfg.py
@@ -1,60 +1,51 @@
 #!/usr/bin python3.6
 
-import asyncio
 import yaml
-from bleak import discover, BleakClient
-from dwm1001_BleApiCommands import DWM1001_BLE_API_COMMANDS
+from dwm1001_BleApi import BleConnectionHandler
+from dwm1001_BleApi import PersistedPositionMsg
+from dwm1001_BleApi import OperationModeMsg
 
-async def getDevices():
-    print('Searching BT devices ...')
-    devices = await discover()
-    return devices
-
-async def readFromDevice(address, UUID):
-    async with BleakClient(address) as client:
-        model_number = await client.read_gatt_char(UUID)
-        print("Model Number: {0}".format(model_number))
-
-async def writeToDevice(address, UUID, data):
-    async with BleakClient(address) as client:
-        success = await client.write_gatt_char(UUID, data)
-         
-def setNodePose(loop, address, pose):
-    pose = b'0000000000000'
-    loop.run_until_complete(writeToDevice(address, DWM1001_BLE_API_COMMANDS.PERSISTED_POSITION, pose))
-         
-if __name__ == "__main__":
-
-    # read BT devs
-    loop = asyncio.get_event_loop()
-    devs = loop.run_until_complete(getDevices())
-    devs_found = {}
-    for dev in devs:
-        devs_found[dev.name] = dev.address
-
-    # load anchors cfg
-    with open("/home/nuc/Documents/dwm_ble_cfg/params/anchors.yaml", 'r') as stream:
+def readYaml(file):
+    with open(file, 'r') as stream:
         try:
-            anchors_cfg = yaml.safe_load(stream)
+            return yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
 
+if __name__ == "__main__":
+
+    # BLE connection handler
+    ble_handler = BleConnectionHandler()
+    
+    # read BT devs
+    devices = ble_handler.getDevices()
+    devices_found = {}
+    for dev in devices:
+        devices_found[dev.name] = dev.address
+
+    # load anchors cfg
+    anchors_cfg = readYaml("/home/nuc/Documents/dwm_ble_cfg/params/anchors.yaml")
+    # load anchors operation mode
+    anchor_operation_mode = readYaml("/home/nuc/Documents/dwm_ble_cfg/params/anchor_operation_mode.yaml")
+    # load tag operation mode
+    tag_operation_mode = readYaml("/home/nuc/Documents/dwm_ble_cfg/params/tag_operation_mode.yaml")
+
     # set some anchor variables
     n_anchors = anchors_cfg['n_anchors']
+    initiator_id = anchors_cfg['initiator_id']
 
-    # devs found info
+    # do stuff with found devices
     for i in range(n_anchors):
         anchor_name = anchors_cfg[f'anchor{i}_id']
-        if anchor_name in devs_found:
-            print(f'{anchor_name} found')
-            print(f'setting configuration ...')
+    
+        if anchor_name in devices_found:
+            print(f'Anchor {anchor_name} found')
             anchor_pose = anchors_cfg[f'anchor{i}_coordinates'].split(', ')
-            #setNodePose(loop, devs_found[anchor_name], 0)
-            #pose = b'\x00\xe8\x03\x00\x00\xd0\x07\x00\x00\xb8\x0b\x00\x00d'
-            loop.run_until_complete(readFromDevice(devs_found[anchor_name], "003bbdf2-c634-4b3d-ab56-7ec889b89a37"))
-            #setNodePose(loop, devs_found[anchor_name], pose)
+            print(f'Setting anchor {anchor_name} pose to [X: {anchor_pose[0]} Y: {anchor_pose[1]} Z: {anchor_pose[2]}]')
+            anchor_pose_msg = PersistedPositionMsg(anchor_pose)
+            ble_handler.send(devices_found[anchor_name], anchor_pose_msg)
+            operation_mode_msg = OperationModeMsg()
+            data = ble_handler.readFromDevice(devices_found[anchor_name], operation_mode_msg.UUID)
+            print(data)
         else:
             print(f'{anchor_name} not found')
-            #bytearray(b'\x00\x808\x01\x00\x808\x01\x00\x808\x01\x00d')
-            #[f'{i:02x}' for i in data]
-            #['00', '80', '38', '01', '00', '80', '38', '01', '00', '80', '38', '01', '00', '64']
